cbf58ed156689d3570a26a5b125302b484fa95a7
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-17 19:07:20 +0300

    adding status variable

--- app/src/main/java/com/grandpa/marvelapp/model/api/response/CharacterResponse.kt
+++ app/src/main/java/com/grandpa/marvelapp/model/api/response/CharacterResponse.kt
@@ -3,6 +3,7 @@ package com.grandpa.marvelapp.model.api.response
 import com.google.gson.annotations.SerializedName
 
 data class CharacterResponse(
+    var status: String,
     @SerializedName("code")
     var responseCode: Int,
     @SerializedName("etag")
