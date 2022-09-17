50561fcb8ec58f97e5b7a3c0bb6abd0b9343d588
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-17 18:48:37 +0300

    adding status code base url and keys to Constants.kt

--- app/src/main/java/com/grandpa/marvelapp/utils/Constants.kt
+++ app/src/main/java/com/grandpa/marvelapp/utils/Constants.kt
@@ -2,18 +2,30 @@ package com.grandpa.marvelapp.utils
 
 class Constants {
 
+    /*
+    these key will be used on each call of the API
 
+     */
     object API_KEY {
         const val PUBLIC_KEY = "26fe45366be9a9a1c5c3374e19f144e6"
         const val PRIVATE_KEY = "a2e22b0ea13a8cbc97b32bd3471f8fbcd807e14c"
     }
 
-
+    /*
+    base api url
+     */
     object DEV_API {
-
-        const val BASE_API = ""
-        const val GET_CHAR = BASE_API + ""
+        const val BASE_API = "http://gateway.marvel.com/v1/public/"
     }
 
+    /*
+    status code of the server
+    the most important ones
+     */
+    object STATUS_CODE {
+        const val STATUS_OK = 200
+        const val NOT_FOUND = 404
+        const val UNAUTHORIZED = 401
+    }
 
 }
