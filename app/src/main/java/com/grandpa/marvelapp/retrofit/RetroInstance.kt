bf7dad8b91ad6c8cf5d880c8441ae85d41692588
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-17 18:49:33 +0300

    update RetroInstance.kt to add okhttpClient

--- app/src/main/java/com/grandpa/marvelapp/retrofit/RetroInstance.kt
+++ app/src/main/java/com/grandpa/marvelapp/retrofit/RetroInstance.kt
@@ -1,6 +1,11 @@
 package com.grandpa.marvelapp.retrofit
 
 import com.grandpa.marvelapp.utils.Constants
+import com.grandpa.marvelapp.utils.Constants.API_KEY.PRIVATE_KEY
+import com.grandpa.marvelapp.utils.utils
+import com.grandpa.marvelapp.utils.utils.md5
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,17 +16,36 @@ class RetroInstance {
 
         /*
         create retrofit instance using gson & RxJava2
+
          */
 
-
         fun getRxRetrofitInstance(): Retrofit {
+            val retrofit = Retrofit.Builder()
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            val httpClient = OkHttpClient.Builder()
 
+            httpClient.addInterceptor(loggingInterceptor)
+            httpClient.addInterceptor { chain ->
+                val original = chain.request()
+                val originalHttpUrl = original.url
+                val url = originalHttpUrl.newBuilder()
+                    .addQueryParameter("apikey", Constants.API_KEY.PUBLIC_KEY)
+                    .addQueryParameter("ts", utils.getTimeStamp())
+                    .addQueryParameter(
+                        "hash",
+                        "${utils.getTimeStamp()}$PRIVATE_KEY${Constants.API_KEY.PUBLIC_KEY}".md5()
+                    )
+                    .build()
+                chain.proceed(original.newBuilder().url(url = url).build())
+            }
 
-            return Retrofit.Builder()
-                .baseUrl(Constants.DEV_API.BASE_API)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
+            retrofit.client(httpClient.build())
+            retrofit.baseUrl(Constants.DEV_API.BASE_API)
+            retrofit.addConverterFactory(GsonConverterFactory.create())
+            retrofit.addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+
+            return retrofit.build()
         }
 
 
