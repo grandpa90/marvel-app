b4188eb01b4962a78bb79ab4322050e6f7814627
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 22:00:43 +0300

    updating ComicsDto.kt to make thumbnail as string

--- app/src/main/java/com/grandpa/marvelapp/model/dto/ComicsDto.kt
+++ app/src/main/java/com/grandpa/marvelapp/model/dto/ComicsDto.kt
@@ -1,10 +1,8 @@
 package com.grandpa.marvelapp.model.dto
 
-import com.grandpa.marvelapp.model.api.Thumbnail
-
 class ComicsDto(
     var id: Long,
     var title: String,
     var description: String,
-    var thumbnail: Thumbnail
+    var thumbnail: String
 )
