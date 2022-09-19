71db19c7f75ac8fb65eb826614947ec46a9da478
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 09:02:44 +0300

    adding stories data & response for API

---
+++ app/src/main/java/com/grandpa/marvelapp/model/api/response/StoriesData.kt
@@ -0,0 +1,11 @@
+package com.grandpa.marvelapp.model.api.response
+
+import com.grandpa.marvelapp.model.api.Stories
+
+data class StoriesData(
+    var offset: Int,
+    var limit: Int,
+    var total: Int,
+    var count: Int,
+    var result: List<Stories>
+)
