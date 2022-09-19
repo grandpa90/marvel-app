bd3a20258890536058685fd529baf6eccd9964bc
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 22:38:30 +0300

    adding toDto function into Comics.kt

--- app/src/main/java/com/grandpa/marvelapp/model/api/Comics.kt
+++ app/src/main/java/com/grandpa/marvelapp/model/api/Comics.kt
@@ -1,6 +1,7 @@
 package com.grandpa.marvelapp.model.api
 
 import com.google.gson.annotations.SerializedName
+import com.grandpa.marvelapp.model.dto.ComicsDto
 
 data class Comics(
     @SerializedName("id")
@@ -8,4 +9,8 @@ data class Comics(
     var title: String,
     var description: String,
     var thumbnail: Thumbnail
-)
+) {
+    fun toComicsDto(): ComicsDto {
+        return ComicsDto(_id, title, description, (thumbnail.path + thumbnail.extension))
+    }
+}
