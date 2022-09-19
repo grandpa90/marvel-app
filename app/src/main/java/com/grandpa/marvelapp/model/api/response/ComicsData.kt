86e65bfe69c498b3c55aeab221fbd75aaf323d0d
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 22:29:36 +0300

    removing Observable into the ComicsData.kt

--- app/src/main/java/com/grandpa/marvelapp/model/api/response/ComicsData.kt
+++ app/src/main/java/com/grandpa/marvelapp/model/api/response/ComicsData.kt
@@ -1,12 +1,11 @@
 package com.grandpa.marvelapp.model.api.response
 
 import com.grandpa.marvelapp.model.api.Comics
-import io.reactivex.Flowable
 
 data class ComicsData(
     var ofset: Int,
     var limit: Int,
     var total: Int,
     var count: Int,
-    var result: Flowable<List<Comics>>
+    var result: List<Comics>
 )
