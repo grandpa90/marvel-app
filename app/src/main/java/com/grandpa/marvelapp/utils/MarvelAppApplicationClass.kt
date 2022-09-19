ff0dcb44468f7c9969df029986ec2f3c0a120592
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 10:52:04 +0300

    updating application class (MarvelAppApplicationClass.kt) to initialize the room db

--- app/src/main/java/com/grandpa/marvelapp/utils/MarvelAppApplicationClass.kt
+++ app/src/main/java/com/grandpa/marvelapp/utils/MarvelAppApplicationClass.kt
@@ -1,10 +1,22 @@
 package com.grandpa.marvelapp.utils
 
 import android.app.Application
+import com.grandpa.marvelapp.roomdb.MarvelRoomDB
 
 class MarvelAppApplicationClass: Application() {
+
+    companion object {
+        // declaring variable of room db
+        lateinit var marvelRoomDB: MarvelRoomDB
+        lateinit var context: MarvelAppApplicationClass
+
+    }
+
     override fun onCreate() {
         super.onCreate()
-        // initialize room db...
+        // initialize room db on application class
+        context = this
+        marvelRoomDB = MarvelRoomDB.getDatabase(context)
+
     }
 }
