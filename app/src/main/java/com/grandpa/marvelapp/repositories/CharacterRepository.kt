3b301f755a396140015edd4343941c7fd93ddf85
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 17:34:59 +0300

    creating CharacterRepository.kt

---
+++ app/src/main/java/com/grandpa/marvelapp/repositories/CharacterRepository.kt
@@ -0,0 +1,134 @@
+package com.grandpa.marvelapp.repositories
+
+import android.app.Application
+import com.grandpa.marvelapp.model.dto.CharacterDto
+import com.grandpa.marvelapp.retrofit.RetroInstance
+import com.grandpa.marvelapp.retrofit.RetroService
+import com.grandpa.marvelapp.roomdb.MarvelRoomDB
+import com.grandpa.marvelapp.roomdb.daos.CharacterDao
+import com.grandpa.marvelapp.roomdb.entities.CharacterEntity
+import io.reactivex.Completable
+import io.reactivex.CompletableObserver
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+class CharacterRepository(application: Application) {
+
+    var characterDao: CharacterDao
+
+    init {
+        val db = MarvelRoomDB.getDatabase(application)
+        characterDao = db.CharacterDao()
+    }
+
+    // insert
+    fun insertCharacter(characterEntity: CharacterEntity) {
+        Completable.create({
+            characterDao.insert(characterEntity)
+        })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : CompletableObserver {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onComplete() {
+                    // disposable.dispose()
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+    }
+
+    // update
+    fun updateCharacer(characterEntity: CharacterEntity) {
+        Completable.create {
+            characterDao.update(characterEntity)
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : CompletableObserver {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onComplete() {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+    }
+
+    fun deleteCharacters() {
+        Completable.create {
+            characterDao.deleteAll()
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : CompletableObserver {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onComplete() {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+            })
+    }
+
+    fun deleteCharacter(_id: Long) {
+        Completable.create {
+            characterDao.deleteCharacter(_id)
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : CompletableObserver {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onComplete() {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+    }
+
+    fun getCharacter(_id: Long): Flowable<CharacterDto> {
+        lateinit var characterDto: CharacterDto
+        val flowableEntity = characterDao.getCharacter(_id = _id)
+        return flowableEntity.map {
+            characterDto = CharacterDto(it._id, it.name, it.description, it.thumbnail)
+            characterDto
+        }
+    }
+
+    fun getCharacters(): Flowable<List<CharacterDto>> {
+        lateinit var characterDto: CharacterDto
+        val flowableEntityList = characterDao.getAllCharacter()
+        return flowableEntityList.map { listEntity ->
+            listEntity.map {
+                characterDto = CharacterDto(it._id, it.name, it.description, it.thumbnail)
+                characterDto
+            }
+        }
+    }
+
+    // online
+    fun getCharactersRemote(): Flowable<List<CharacterDto>> {
+        lateinit var characterDto: CharacterDto
+        val retroInstance = RetroInstance.getRxRetrofitInstance().create(RetroService::class.java)
+        val flowableList = retroInstance.getCharacters()
+        return flowableList.map {
+            it.data.result
+                .map { character ->
+                    characterDto = character.characterToDto()
+                    // here we need to insert
+                    characterDto
+                }
+        }
+    }
+
+}
