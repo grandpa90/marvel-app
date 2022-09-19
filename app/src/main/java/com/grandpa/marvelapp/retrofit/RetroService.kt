30f5b1922b35a6988a9dad13f9ab4277b6d2355e
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 12:09:19 +0300

    adding RetroService.kt functions

--- app/src/main/java/com/grandpa/marvelapp/retrofit/RetroService.kt
+++ app/src/main/java/com/grandpa/marvelapp/retrofit/RetroService.kt
@@ -1,10 +1,26 @@
 package com.grandpa.marvelapp.retrofit
 
+import com.grandpa.marvelapp.model.api.response.*
+import io.reactivex.Flowable
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RetroService {
 
     @GET("characters")
-    fun getCharacters()
+    fun getCharacters(): Flowable<CharacterResponse>
+
+    @GET("characters/{characterId}/comics")
+    fun getComics(@Path("characterId") characterId: Long): Flowable<ComicsResponse>
+
+    @GET("character/{characterId}/stories")
+    fun getStories(@Path("characterId") characterId: Long): Flowable<StoriesResponse>
+
+    @GET("character/{characterId}/events")
+    fun getEvents(@Path("characterId") characterId: Long): Flowable<EventsResponse>
+
+    @GET("character/{characterId}/series")
+    fun getSeries(@Path("characterId") characterId: Long): Flowable<SeriesResponse>
+
 
 }
