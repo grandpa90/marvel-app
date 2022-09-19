3f1aaf7758a1df53c0411ac2c9be487b9a707d4b
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 17:36:59 +0300

    updating CharacterViewModel.kt

--- app/src/main/java/com/grandpa/marvelapp/viewModel/CharacterViewModel.kt
+++ app/src/main/java/com/grandpa/marvelapp/viewModel/CharacterViewModel.kt
@@ -1,4 +1,59 @@
 package com.grandpa.marvelapp.viewModel
 
-class CharacterViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.grandpa.marvelapp.model.dto.CharacterDto
+import com.grandpa.marvelapp.repositories.CharacterRepository
+import com.grandpa.marvelapp.roomdb.entities.CharacterEntity
+import io.reactivex.Flowable
+
+class CharacterViewModel(application: Application) : AndroidViewModel(application) {
+    var characterRepository: CharacterRepository
+
+    init {
+        characterRepository = CharacterRepository(application)
+    }
+
+    fun insertCharacter(characterDto: CharacterDto) {
+        characterRepository.insertCharacter(
+            CharacterEntity(
+                characterDto._id,
+                characterDto.name,
+                characterDto.description,
+                characterDto.thumbnail
+            )
+        )
+    }
+
+    fun updateCharacter(characterDto: CharacterDto) {
+        characterRepository.updateCharacer(
+            CharacterEntity(
+                characterDto._id,
+                characterDto.name,
+                characterDto.description,
+                characterDto.thumbnail
+            )
+        )
+    }
+
+    fun deleteAllCharacters() {
+        characterRepository.deleteCharacters()
+    }
+
+    fun deleteCharacter(characterDto: CharacterDto) {
+        characterRepository.deleteCharacter(characterDto._id)
+    }
+
+    fun getCharacter(_id: Long): Flowable<CharacterDto> {
+        return characterRepository.getCharacter(_id)
+    }
+
+    fun getCharacters(): Flowable<List<CharacterDto>> {
+        return characterRepository.getCharacters()
+    }
+
+    fun getCharactersRemote(): Flowable<List<CharacterDto>> {
+        return characterRepository.getCharactersRemote()
+    }
+
 }
