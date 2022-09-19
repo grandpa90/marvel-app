62ad15571a91af43d2bbd214fc4134061f4f263c
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-19 09:03:38 +0300

    adding comics and series dto

--- app/src/main/java/com/grandpa/marvelapp/model/dto/ComicsDto.kt
+++ app/src/main/java/com/grandpa/marvelapp/model/dto/ComicsDto.kt
@@ -3,7 +3,6 @@ package com.grandpa.marvelapp.model.dto
 import com.grandpa.marvelapp.model.api.Thumbnail
 
 class ComicsDto(
-
     var id: Long,
     var title: String,
     var description: String,
---
+++ app/src/main/java/com/grandpa/marvelapp/model/dto/SeriesDto.kt
@@ -0,0 +1,8 @@
+package com.grandpa.marvelapp.model.dto
+
+class SeriesDto(
+    var _id: Long,
+    var title: String,
+    var description: String,
+    var thumbnail: ThumbnailDto
+)
