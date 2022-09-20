a2dd48c65db47cfb4a32914510e12472ad4f93eb
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-20 23:44:41 +0300

    adding stories repo and view model

---
+++ app/src/main/java/com/grandpa/marvelapp/repositories/StoriesRepository.kt
@@ -0,0 +1,111 @@
+package com.grandpa.marvelapp.repositories
+
+import android.app.Application
+import com.grandpa.marvelapp.model.dto.StoriesDto
+import com.grandpa.marvelapp.retrofit.RetroInstance
+import com.grandpa.marvelapp.retrofit.RetroService
+import com.grandpa.marvelapp.roomdb.MarvelRoomDB
+import com.grandpa.marvelapp.roomdb.daos.StoriesDao
+import com.grandpa.marvelapp.roomdb.entities.StoriesEntity
+import io.reactivex.Completable
+import io.reactivex.CompletableObserver
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+class StoriesRepository(application: Application) {
+    private var storiesDao: StoriesDao
+    private var db: MarvelRoomDB
+
+    init {
+        db = MarvelRoomDB.getDatabase(application)
+        storiesDao = db.StoriesDao()
+    }
+
+    fun insertStory(storiesDto: StoriesDto) {
+        Completable.create {
+            storiesDao.insert(
+                StoriesEntity(
+                    storiesDto._id, storiesDto.description,
+                    storiesDto.description, storiesDto.thumbnail
+                )
+            )
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : CompletableObserver {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onComplete() {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+            })
+    }
+
+    fun updateStory(storiesDto: StoriesDto) {
+        Completable.create {
+            storiesDao.update(
+                StoriesEntity(
+                    storiesDto._id, storiesDto.description,
+                    storiesDto.description, storiesDto.thumbnail
+                )
+            )
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : CompletableObserver {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onComplete() {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+            })
+    }
+
+    fun getStories(): Flowable<List<StoriesDto>> {
+        lateinit var storiesDto: StoriesDto
+        val flowableList = storiesDao.getAllStories()
+        return flowableList.map {
+            it.map { storyEntity ->
+                storiesDto = storyEntity.toStoriesDto()
+                storiesDto
+            }
+        }
+    }
+
+    fun getStory(_id: Long): Flowable<StoriesDto> {
+        lateinit var storiesDto: StoriesDto
+        val flowable = storiesDao.getStories(_id)
+        return flowable.map {
+            storiesDto = it.toStoriesDto()
+            storiesDto
+        }
+    }
+
+
+    fun getStoriesRemote(characterId: Long): Flowable<List<StoriesDto>> {
+        lateinit var storiesDto: StoriesDto
+        val retroInstance = RetroInstance.getRxRetrofitInstance().create(RetroService::class.java)
+        val flowableList = retroInstance.getStories(characterId = characterId)
+        return flowableList.map {
+            it.data.result.map { stories ->
+                storiesDto = stories.toStoriesDto()
+                storiesDao.insert(
+                    StoriesEntity(
+                        storiesDto._id, storiesDto.title,
+                        storiesDto.description, storiesDto.thumbnail
+                    )
+                )
+                storiesDto
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/grandpa/marvelapp/viewModel/StoriesViewModel.kt
+++ app/src/main/java/com/grandpa/marvelapp/viewModel/StoriesViewModel.kt
@@ -1,4 +1,37 @@
 package com.grandpa.marvelapp.viewModel
 
-class StoriesViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.grandpa.marvelapp.model.dto.StoriesDto
+import com.grandpa.marvelapp.repositories.StoriesRepository
+import io.reactivex.Flowable
+
+class StoriesViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var storiesRepository: StoriesRepository
+
+    init {
+        storiesRepository = StoriesRepository(application)
+    }
+
+    fun insertStory(storiesDto: StoriesDto) {
+        storiesRepository.insertStory(storiesDto)
+    }
+
+    fun updateStory(storiesDto: StoriesDto) {
+        storiesRepository.updateStory(storiesDto)
+    }
+
+    fun getAllStories(): Flowable<List<StoriesDto>> {
+        return storiesRepository.getStories()
+    }
+
+    fun getStory(_id: Long): Flowable<StoriesDto> {
+        return storiesRepository.getStory(_id)
+    }
+
+    fun getStoriesRemote(characterId: Long): Flowable<List<StoriesDto>> {
+        return storiesRepository.getStoriesRemote(characterId)
+    }
+
 }
