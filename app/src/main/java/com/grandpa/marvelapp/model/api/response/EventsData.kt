3b4eed4c0660066f028214cd3fce9a4e2e22a683
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-21 22:37:47 +0300

    remove flowable from data

--- app/src/main/java/com/grandpa/marvelapp/model/api/response/EventsData.kt
+++ app/src/main/java/com/grandpa/marvelapp/model/api/response/EventsData.kt
@@ -1,12 +1,11 @@
 package com.grandpa.marvelapp.model.api.response
 
 import com.grandpa.marvelapp.model.api.Events
-import io.reactivex.Flowable
 
 data class EventsData(
     var offset: Int,
     var limit: Int,
     var total: Int,
     var count: Int,
-    var result: Flowable<List<Events>>
+    var result: List<Events>
 )
