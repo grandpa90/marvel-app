37ffc495dff383c76316e54ffd16f3d51e4bf4e9
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-21 22:39:20 +0300

    finishing view models

--- app/src/main/java/com/grandpa/marvelapp/viewModel/EventsViewModel.kt
+++ app/src/main/java/com/grandpa/marvelapp/viewModel/EventsViewModel.kt
@@ -1,4 +1,45 @@
 package com.grandpa.marvelapp.viewModel
 
-class EventsViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.grandpa.marvelapp.model.dto.EventsDto
+import com.grandpa.marvelapp.repositories.EventsRepository
+import io.reactivex.Flowable
+
+class EventsViewModel(application: Application) : AndroidViewModel(application) {
+
+    var eventsRepository: EventsRepository
+
+    init {
+        eventsRepository = EventsRepository(application)
+    }
+
+    fun insertEvent(eventsDto: EventsDto) {
+        eventsRepository.insertEvents(eventsDto)
+    }
+
+    fun updateEvent(eventsDto: EventsDto) {
+        eventsRepository.updateEvents(eventsDto)
+    }
+
+    fun deleteEvent(_id: Long) {
+        eventsRepository.deleteEvent(_id)
+    }
+
+    fun deleteEvents() {
+        eventsRepository.deleteEvents()
+    }
+
+    fun getAllEvents(): Flowable<List<EventsDto>> {
+        return eventsRepository.getAllEvents()
+    }
+
+    fun getEvent(_id: Long): Flowable<EventsDto> {
+        return eventsRepository.getEvent(_id)
+    }
+
+    fun getEventsRemote(characterId: Long): Flowable<List<EventsDto>> {
+        return eventsRepository.getEventsRemote(characterId)
+    }
+
 }
--- app/src/main/java/com/grandpa/marvelapp/viewModel/SeriesViewModel.kt
+++ app/src/main/java/com/grandpa/marvelapp/viewModel/SeriesViewModel.kt
@@ -1,4 +1,44 @@
 package com.grandpa.marvelapp.viewModel
 
-class SeriesViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.grandpa.marvelapp.model.dto.SeriesDto
+import com.grandpa.marvelapp.repositories.SeriesRepository
+import io.reactivex.Flowable
+
+class SeriesViewModel(application: Application) : AndroidViewModel(application) {
+    var seriesRepository: SeriesRepository
+
+    init {
+        seriesRepository = SeriesRepository(application)
+    }
+
+    fun insertSeries(seriesDto: SeriesDto) {
+        seriesRepository.insertSeries(seriesDto)
+    }
+
+    fun updateSeries(seriesDto: SeriesDto) {
+        seriesRepository.updateSeries(seriesDto)
+    }
+
+    fun deleteSeries(_id: Long) {
+        seriesRepository.deleteSeries(_id)
+    }
+
+    fun deleteAllSeries() {
+        seriesRepository.deleteAllSeries()
+    }
+
+    fun getAllSeries(): Flowable<List<SeriesDto>> {
+        return seriesRepository.getAllSeries()
+    }
+
+    fun getSeries(_id: Long): Flowable<SeriesDto> {
+        return seriesRepository.getSeries(_id)
+    }
+
+    fun getSeriesRemote(characterId: Long): Flowable<List<SeriesDto>> {
+        return seriesRepository.getSeriesRemote(characterId)
+    }
+
 }
