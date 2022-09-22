568c304468e038313004faa0b185bac1c21298ce
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-22 19:20:27 +0300

    adding DetailsActivity.kt

---
+++ app/src/main/java/com/grandpa/marvelapp/activities/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.grandpa.marvelapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.grandpa.marvelapp.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
--- app/src/main/java/com/grandpa/marvelapp/activities/MainActivity.kt
+++ app/src/main/java/com/grandpa/marvelapp/activities/MainActivity.kt
@@ -2,13 +2,21 @@ package com.grandpa.marvelapp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.grandpa.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initializeNewSplash()
         setContentView(R.layout.activity_main)
+
+
     }
 
+    private fun initializeNewSplash() {
+        val splash = installSplashScreen()
+
+    }
 
 }
