5c09468e3469c5a41061a4469ef098b3c6babc38
zakaria darwish <zakaria.safi.darwish@gmail.com>
2022-09-22 17:13:38 +0300

    moving MainActivity.kt to activities package

--- app/src/main/java/com/grandpa/marvelapp/activities/MainActivity.kt
+++ app/src/main/java/com/grandpa/marvelapp/activities/MainActivity.kt
@@ -1,16 +1,14 @@
-package com.grandpa.marvelapp
+package com.grandpa.marvelapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.grandpa.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 
 
 }
